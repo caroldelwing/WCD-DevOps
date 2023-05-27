3561328ecb218f9d798815f084cb9412c3f0779a
Carolina Delwing Rosa <caroldelwing@hotmail.com>
2023-05-26 21:16:21 -0400

    Added main and aux scripts

---
+++ project_1/user-data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Update and upgrade packages
+sudo apt update -y
+sudo apt upgrade -y
+
+#Install Python3
+sudo apt install python3
+
+#Download and install Nodejs 18, install npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y npm
+
+#Install Java
+sudo apt install -y openjdk-11-jre-headless
+
+#Install Docker (not working)
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
