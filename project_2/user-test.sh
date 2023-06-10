b74afdbd3a01341c3a243d6df50a9b7c848d7c3d
Carolina Delwing Rosa <caroldelwing@hotmail.com>
2023-06-10 13:27:28 -0400

    Added main script, database spreadsheet, and an nginx test user-data script

---
+++ project_2/user-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+# Install Nginx
+sudo apt install -y nginx
+
+# Start Nginx
+sudo systemctl start nginx
+
+# Enable Nginx to start on boot
+sudo systemctl enable nginx
