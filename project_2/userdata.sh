f28c789ee6226a2a6a62fa709c1b16117250b237
beatrizCarvalhoOliveira <your_email@example.com>
2023-06-10 11:45:47 -0400

    commit message

--- project_2/userdata.sh
+++ project_2/userdata.sh
@@ -10,8 +10,8 @@ sudo apt-get install gnupg
 #Import public key used by apt
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
-#List File for mongoDB 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+#List File for mongoDB (ubuntu 20)
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.com/apt/ubuntu focal/mongodb-enterprise/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
 
 #Update and upgrade packages
 sudo apt update -y
