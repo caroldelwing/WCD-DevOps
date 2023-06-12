82c5b210f86025577bd1d18861c5c334dced764c
Carolina Delwing Rosa <caroldelwing@hotmail.com>
2023-06-12 19:42:54 -0400

    DB instance IP added

--- project_2/userdata_app.sh
+++ project_2/userdata_app.sh
@@ -4,4 +4,4 @@ sudo apt install -y curl git nodejs npm
 git clone https://github.com/caroldelwing/WCD-DevOps.git
 cd WCD-DevOps/project_2
 npm install
-TESTDB_HOST=testdb DB_HOST= DB_PORT=27017 DB_NAME=WCD_project2 npm run start:dev
+TESTDB_HOST=testdb DB_HOST=10.0.10.10 DB_PORT=27017 DB_NAME=WCD_project2 npm run start:dev
