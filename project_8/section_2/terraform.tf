267778a96e396e3105354a7cdd3a25c027dbbf2c
Carolina Delwing Rosa <caroldelwing@hotmail.com>
2023-08-12 10:41:28 -0400

    first commit of project 8, uncompleted

---
+++ project_8/section_2/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
